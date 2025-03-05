0a1e399f3d35452be9335c734b226bbef047974b
daosgava <daosgava.garcia@gmail.com>
2025-03-05 22:38:53 +1100

    add contact email

--- src/modules/contact.ts
+++ src/modules/contact.ts
@@ -2,7 +2,7 @@ const contactMarkup = `
   <section id="contact" class="section-container">
     <h2 class="subtitle">Let's Talk!</h2>
     <div>
-      <a class="email" href="mailto:info@danielgarcia.codes">info@danielgarcia.codes</a>
+      <a class="email" href="mailto:hello@danielgarcia.codes">hello@danielgarcia.codes</a>
       <hr>
     </div>
   </section>
