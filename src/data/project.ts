9a44822e8c3b9d349a2091f39ed7114d062e08e5
daosgava <daosgava.garcia@gmail.com>
2025-03-06 11:47:52 +1100

    move tech logos, start projects section

---
+++ src/data/project.ts
@@ -0,0 +1,25 @@
+const PROJECTS = [
+  {
+    name: "Opticity Dashboard",
+    description: "",
+    images: [],
+    url: "",
+    github: "",
+  },
+  {
+    name: "Interactive Street View & Map Navigation",
+    description: "",
+    images: [],
+    url: "",
+    github: "",
+  },
+  {
+    name: "C# WebAssembly Compiler: A Browser-Based Solution",
+    description: "",
+    images: [],
+    url: "",
+    github: "",
+  },
+];
+
+export { PROJECTS };
--- src/modules/projects.ts
+++ src/modules/projects.ts
@@ -2,6 +2,12 @@ const projectMarkup = `
   <section id="projects" class="section-container">
     <h2 class="subtitle">Projects<hr></h2>
     <div class="projects-content">
+      <div class="menu">
+
+     </div>
+     <div class="project">
+
+     <div>
     </div>
   </section>
 `;
