a759bbe4bc656d75042875fbaa87cad27004df03
daosgava <daosgava.garcia@gmail.com>
2025-03-10 02:34:37 +1100

    add mobile menu, burger icon

--- src/modules/menu.ts
+++ src/modules/menu.ts
@@ -1,8 +1,28 @@
 import { modeSwitchMarkup } from "./modeSwitch";
 import logo from "../images/logo.png";
 
+const MobileMenuMarkup = `
+  <nav class="mobile-menu hidden">
+    <a class="menu-item" href="#about">Home</a>
+    <a class="menu-item" href="#about">About</a>
+    <a class="menu-item" href="#experience">Experience</a>
+    <a class="menu-item" href="#projects">Projects</a>
+    <a class="menu-item" href="#contact">Contact</a>
+    <button class="close-mobile-menu">x</button>
+  </nav>
+`;
+
+const burger = `
+  <div class="burger-menu">
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+`;
+
 const menuMarkup = `
   <nav class="menu">
+    ${burger}
     <a class="menu-item logo" href="#home"><img src="${logo}" alt="Daniel Garcia - Software Developer"/></a>
     <a class="menu-item" href="#about">About</a>
     <a class="menu-item" href="#experience">Experience</a>
@@ -10,7 +30,25 @@ const menuMarkup = `
     <a class="menu-item" href="#contact">Contact</a>
     ${modeSwitchMarkup}
   </nav>
+  ${MobileMenuMarkup}
 `;
+
+const initializeMobileMenu = () => {
+  const burgerMenu = document.querySelector(".burger-menu");
+  const menuContainer = document.querySelector(".menu");
+  const mobileMenu = document.querySelector(".mobile-menu");
+
+  burgerMenu?.addEventListener("click", () => {
+    menuContainer?.classList.toggle("hidden");
+    mobileMenu?.classList.toggle("hidden");
+  });
+
+  const closeMobileMenu = document.querySelector(".close-mobile-menu");
+  closeMobileMenu?.addEventListener("click", () => {
+    menuContainer?.classList.remove("hidden");
+    mobileMenu?.classList.add("hidden");
+  })
+};
 
 const initializeMenuItems = () => {
   const menuItems = document.querySelectorAll(".menu-item");
@@ -32,9 +70,16 @@ const initializeMenuItems = () => {
     item.addEventListener("click", () => {
       menuItems.forEach((item: Element) => {
         item.classList.remove("blur");
+        const mobileMenu = document.querySelector(".mobile-menu");
+        const menuContainer = document.querySelector(".menu");
+
+        mobileMenu?.classList.add("hidden");
+        menuContainer?.classList.remove("hidden");
       });
-    })
+    });
   });
+
+  initializeMobileMenu();
 };
 
-export { initializeMenuItems, menuMarkup };
+export { initializeMenuItems, menuMarkup, MobileMenuMarkup };
